582ab7b0e60bf2c651ea4886dcabdeb3519ded40
It's.Danger <121919835+Its-Danger@users.noreply.github.com>
2024-10-25 03:11:46 +0530

    update 01

--- Chatterbot.py
+++ Chatterbot.py
@@ -6,10 +6,12 @@ chatbot = ChatBot('FreeBirdsBot')
 
 trainer = ListTrainer(chatbot)
 
-trainer.train(['Hi','Hello','How are you?','I am fine and You?','Greate','What are you Doing?','nothing just roaming around.'])
+trainer.train(['Hi','fucku','How are you?','I am fine and You?','Greate','What are you Doing?','nothing just roaming around.'])
 
 while True:
 	input_data = input("You- ")
 	response = chatbot.get_response(input_data)
 	print("FreeBirdsBot- ",response)
 
+ 
+print("hi")
